3ea23bcb49b75b769677a5b7592abd57ba7e8ae5
Chiranjeevi005 <chiranjeevi8050@gmail.com>
2025-09-20 20:07:05 +0530

    Deploment Error Fixation

---
+++ backend/jest.config.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    roots: ['<rootDir>/tests'],
+    testMatch: ['**/*.test.ts'],
+    collectCoverageFrom: [
+        'src/**/*.ts',
+        '!src/**/*.d.ts',
+        '!src/server.ts',
+        '!src/config/**',
+    ],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text', 'lcov', 'html'],
+    setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+};
+exports.default = config;
